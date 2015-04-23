225ec0f69bcf2ce99d4b61e688b6e739c84b489c
BoboTiG <bobotig@gmail.com>
2015-04-23 14:12:33 +0200

    Utilisation du script fourni par Pi-hole

--- update-rstop.sh
+++ update-rstop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+echo 'Getting RStop list ...'
 sudo wget -q -nv 'https://raw.githubusercontent.com/BoboTiG/rstop-list/master/rstop.conf' -O /etc/dnsmasq.d/RStop.conf
-sudo wget -q -nv 'http://pgl.yoyo.org/as/serverlist.php?hostformat=dnsmasq&showintro=0&mimetype=plaintext' -O /etc/dnsmasq.d/pgl.yoyo.org.conf
-sudo service dnsmasq restart
+sudo /usr/local/bin/gravity.sh
 
 exit 0
